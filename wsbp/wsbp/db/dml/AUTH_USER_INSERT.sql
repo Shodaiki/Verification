c3df71a3ecc58fbea4559c2d3d6ed55632103f49
Dshoji <b221.d.shoji@gmail.com>
2024-12-09 00:05:55 +0900

    テストの前後の沿å処理を追加

---
+++ wsbp/wsbp/db/dml/AUTH_USER_INSERT.sql
@@ -0,0 +1,4 @@
+INSERT INTO
+    auth_user(user_name, user_pass)
+VALUES
+    (b1111111, b1111111)
--- wsbp/wsbp/src/test/java/com/example/wsbp/service/IUserServiceTest.java
+++ wsbp/wsbp/src/test/java/com/example/wsbp/service/IUserServiceTest.java
@@ -1,10 +1,13 @@
 package com.example.wsbp.service;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,6 +17,23 @@ public class IUserServiceTest {
 
     @Autowired
     IUserService userService;
+
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+
+    @BeforeEach
+    public void setUp() {
+        // 必要に応じて、テスト用のデータを準備
+        jdbcTemplate.update("DELETE FROM auth_user WHERE user_name = ?", "b0000000");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        // テストで挿入されたデータを削除
+        jdbcTemplate.update("DELETE FROM auth_user WHERE user_name = ?", "b0000000");
+    }
 
     @Test
     @DisplayName("名前とパスワードを引数として送ると、記録された行数（1行）が戻り値として返ってくる")
@@ -32,8 +52,8 @@ public class IUserServiceTest {
     @Test
     @DisplayName("userNameとuserPassを入力値として送ると、正常に記録され記録行数（1行）が返ってくる")
     public void TestWhite01(){
-        String userName = "b1111111";
-        String password = "b1111111";
+        String userName = "b0000000";
+        String password = "b0000000";
 
         int expected = 1;
 
@@ -47,7 +67,7 @@ public class IUserServiceTest {
     @DisplayName("userNameの値が空白またはnullのとき、例外がスローされる")
     public void TestWhite02(){
         String userName = null;
-        String password = "b2222222";
+        String password = "b0000000";
 
         assertThrows(DataAccessException.class, () -> userService.registerUser(userName, password));
     }
@@ -56,7 +76,7 @@ public class IUserServiceTest {
     @Test
     @DisplayName("userPassの値が空白またはnullのとき、例外がスローされる")
     public void TestWhite03(){
-        String userName = "b3333333";
+        String userName = "b0000000";
         String password = null;
 
         assertThrows(DataAccessException.class, () -> userService.registerUser(userName, password));
